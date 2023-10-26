fe93d6d7d0874191e59585663067fde405bf1b63
parkkyungnam <pj.pkn@kalmate.net>
2023-10-26 14:10:16 +0900

    Step

---
+++ backend/UserModule/src/main/java/org/example/UserController.java
@@ -0,0 +1,17 @@
+/*
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
+*/
---
+++ backend/UserModule/src/main/java/org/example/UserModuleServer.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserModuleServer {
+    public static void main(String[] args) {
+        SpringApplication.run(UserModuleServer.class, args);
+    }
+
+}
